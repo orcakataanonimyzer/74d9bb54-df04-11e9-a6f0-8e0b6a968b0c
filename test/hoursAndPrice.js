99e94cfd267e457bc8dc4e0a144a18573190beb3
someone <something@apa.com>
2019-09-17 17:26:04 -0500

    comment about why arrays are used

--- test/hoursAndPrice.js
+++ test/hoursAndPrice.js
@@ -3,10 +3,14 @@ const main = require('../index');
 
 //check to make sure that we get an array of keys from our object
 describe('We can extract array of hours and price per hour from our family object', () => {
+  //using array because we'll easily be able to loop through them and return key*value
   it('should return an array for the hours worked', () => {
     expect(main.keysOfObjects(main.A)).to.be.an('array');
+    expect(main.keysOfObjects(main.C)).to.be.an('array');
   });
+
   it('should return an array for the price per hour', () => {
-    expect(main.valuesOfObjects(main.A)).to.be.an('array');
+    expect(main.valuesOfObjects(main.B)).to.be.an('array');
+    expect(main.valuesOfObjects(main.C)).to.be.an('array');
   });
 });
