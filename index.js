22ee18b9f7331617018b1f692966dac1d410d7e0
someone <something@apa.com>
2019-09-17 16:08:32 -0500

    family and rate/ hours added as map

---
+++ index.js
@@ -0,0 +1,6 @@
+//11 hours per family
+//using an object because it makes sense in mapping hours to rates
+
+const A = { 6: 15, 5: 20 };
+const B = { 5: 12, 2: 8, 4: 16 };
+const C = { 4: 21, 7: 15 };
