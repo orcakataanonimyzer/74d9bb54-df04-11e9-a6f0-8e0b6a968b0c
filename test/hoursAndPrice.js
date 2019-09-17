d553b41024b3cd630f79e067f655d2681dcf1379
someone <something@apa.com>
2019-09-17 17:19:55 -0500

    tests for hours and prices arrays failing

---
+++ test/hoursAndPrice.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const main = require('../index');
+
+//check to make sure that we get an array of keys from our object
+describe('We can extract array of hours and price per hour from our family object', () => {
+  it('should return an array for the hours worked', () => {
+
+    expect(fn(main.A)).to.be.an(array)
+  });
+  it('should return an array for the price per hour', () => {
+
+    expect(fnmain.A)).to.be.an(array)
+  });
+});
